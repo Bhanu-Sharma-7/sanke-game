85e8be24ef38cfa7b12cf196b635ccd12718d200
Bhanu-Sharma-7 <bhanusharma14581@gmail.com>
2026-01-05 20:07:16 +0530

    food, snack completed

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const blockWidth = 50
 const cols = Math.floor(board.clientWidth / blockWidth)
 const rows = Math.floor(board.clientHeight / blockHeight)
 const blocks = []
-const snake = [ {
+const snake = [{
     x: 1, y: 3
-} ]
-let direction = 'down'
+}]
+
+let direction = 'right'
+let intervalId = null
+let food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
 
 for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
@@ -19,39 +22,53 @@ for (let row = 0; row < rows; row++) {
 }
 
 function render() {
-    snake.forEach(segment=> {
-        blocks[`${segment.x},${segment.y}`].classList.add('fill')
-    })
-}
-
-setInterval(() => {
-    
     let head = null
-    if(direction === 'left') {
+
+    blocks[`${food.x},${food.y}`].classList.add('food')
+
+    if (direction === 'left') {
         head = { x: snake[0].x, y: snake[0].y - 1 }
-    } else if(direction === 'right') {
+    } else if (direction === 'right') {
         head = { x: snake[0].x, y: snake[0].y + 1 }
-    } else if(direction === 'up') {
+    } else if (direction === 'up') {
         head = { x: snake[0].x - 1, y: snake[0].y }
-    } else if(direction === 'down') {
+    } else if (direction === 'down') {
         head = { x: snake[0].x + 1, y: snake[0].y }
     }
-    snake.forEach(segment=> {
+
+    if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
+        alert('Game Over')
+        clearInterval(intervalId)
+    }
+
+    if (head.x === food.x && head.y === food.y) {
+        blocks[`${food.x},${food.y}`].classList.remove('food')
+        snake.unshift(head)
+        food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
+    }
+
+    snake.forEach(segment => {
         blocks[`${segment.x},${segment.y}`].classList.remove('fill')
     })
     snake.unshift(head)
     snake.pop()
+    snake.forEach(segment => {
+        blocks[`${segment.x},${segment.y}`].classList.add('fill')
+    })
+}
+
+intervalId = setInterval(() => {
     render()
-}, 600);
+}, 100);
 
 addEventListener("keydown", (event) => {
-    if(event.key === 'ArrowLeft') {
+    if (event.key === 'ArrowLeft') {
         direction = 'left'
-    } else if(event.key === 'ArrowRight') {
+    } else if (event.key === 'ArrowRight') {
         direction = 'right'
-    } else if(event.key === 'ArrowUp') {
+    } else if (event.key === 'ArrowUp') {
         direction = 'up'
-    } else if(event.key === 'ArrowDown') {
+    } else if (event.key === 'ArrowDown') {
         direction = 'down'
     }
 })
