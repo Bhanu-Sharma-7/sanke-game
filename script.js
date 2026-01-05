e414247adc22444f8cb5bd4b8a40df65d92c0bcf
Bhanu-Sharma-7 <bhanusharma14581@gmail.com>
2026-01-05 15:28:09 +0530

    building snack

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Hello World")
+const board = document.querySelector('.board')
+const blockHeight = 50
+const blockWidth = 50
+const cols = Math.floor(board.clientWidth / blockWidth)
+const rows = Math.floor(board.clientHeight / blockHeight)
+const blocks = []
+const snake = [ {
+    x: 1, y: 3
+}, {
+    x: 1, y: 4
+}, {
+    x: 1, y: 5
+} ]
+
+for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        board.appendChild(block)
+        block.innerText = `${col},${row}`
+        blocks[`${col},${row}`] = block
+    }
+}
+
+function render() {
+
+    snake.forEach(segment=> {
+        console.log(segment)
+    })
+}
